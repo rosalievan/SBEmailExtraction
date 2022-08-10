79d53f25c861ed5b7d18753f3df8a3e01b950acc
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-10 19:39:28 +0100

    part1 naive

---
+++ emailextraction.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+input = fs.readFileSync("test.txt", 'utf8')
+console.log(input)
+
+var counter = 0
+ 
+for(var i = 0; i < input.length; i++) {
+  if (input.substring(i, i+ 13) == '@softwire.com') { counter = counter + 1 }
+}
+ 
+console.log(counter)
