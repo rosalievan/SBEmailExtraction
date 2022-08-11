a1dd0443cf9fe177413a894cf4cc4c9be4ad9421
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-11 10:51:26 +0100

    not working, pushing for collab

--- emailextractiondictionary.js
+++ emailextractiondictionary.js
@@ -2,3 +2,29 @@ const fs = require('fs')
 
 input = fs.readFileSync("test.txt", 'utf8')
 
+let emailDictionary = {}
+let domains = []
+let usedDomains = []
+
+let pattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+$)/gim;
+let email_addresses = input.match(pattern)
+
+for (key in email_addresses){
+    domains.push(email_addresses[key].split('@')[1])
+}
+
+
+for (item in domains){
+    let domain = domains[item]
+    console.log(domain)
+    if (domain in usedDomains){
+        emailDictionary[domain] += 1
+    } else {
+        emailDictionary[domain] = 1
+        usedDomains.push(emailDictionary[domain])
+    }
+}
+
+
+console.log(emailDictionary)
+// console.log(email_addresses.match(pattern2))
