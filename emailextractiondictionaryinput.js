4b7d5407a14d357a7f5d23b47ff112f33273cb1b
rosalievan <r.vanonzenoort98@gmail.com>
2022-08-11 11:59:55 +0100

    making code into functions

--- emailextractiondictionaryinput.js
+++ emailextractiondictionaryinput.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 
 input = fs.readFileSync("test.txt", 'utf8')
 
-function returnEmailDict(input){
 let emailDictionary = {}
 let domains = []
 let usedDomains = []
@@ -14,8 +13,7 @@ for (key in email_addresses){
     domains.push(email_addresses[key].split('@')[1])
 }
 
-// console.log(Array.isArray(domains))
-
+console.log(domains.length)
 for (item in domains){
     let domain = domains[item]
     if (usedDomains.includes(domain)){
@@ -26,32 +24,19 @@ for (item in domains){
     }
 }
 
-return emailDictionary
-}
-
-emailDictionary = returnEmailDict(input)
 console.log(emailDictionary)
 
 
 // printing top 10 in order
-function printTop10 (emailDictionary) {
-
-let output = []
-
 var items = Object.keys(emailDictionary).map(function(key) {
     return [key, emailDictionary[key]];
   });
-
+  
 items.sort(function(first, second) {
 return second[1] - first[1];
 });
 
 top10 = items.slice(0, 10);
 for (item in top10){
-    output.push(top10[item][0])
+    console.log(top10[item][0])
 }
-return output
-
-}
-
-console.log(printTop10(emailDictionary))
